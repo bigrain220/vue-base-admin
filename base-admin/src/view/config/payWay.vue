8673fef7a4b2e9dc030a867f95b2fe7372f051b4
william <1573223470@qq.com>
2020-07-09 10:03:46 +0800

    'slot'

--- base-admin/src/view/config/payWay.vue
+++ base-admin/src/view/config/payWay.vue
@@ -3,20 +3,29 @@
     <content-header :title="title"></content-header>
     <div class="content-con">
       <div class="pay-way-box every-page-box">
-        <el-button type="primary" size="medium " class="add-btn" @click="addClick">新增</el-button>
-        <el-form :inline="true" :model="searchForm" class="search-form" label-width="60px" size="medium">
-          <el-form-item label>
+        <el-form :inline="true" ref="searchForm" :model="searchForm" class="search-form" label-width="60px" size="medium">
+          <el-form-item>
             <el-input placeholder="请输入查找内容" v-model="searchForm.value" class="input-with-select" @keydown.enter.native="search(true)" clearable>
               <el-select v-model="searchForm.name" slot="prepend">
                 <el-option label="名称" value="name"></el-option>
+                <el-option label="ID" value="id"></el-option>
               </el-select>
             </el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="status">
+            <el-select v-model="searchForm.status" placeholder="请选择状态">
+              <el-option label="正常" :value="'1'"></el-option>
+              <el-option label="禁用" :value="'2'"></el-option>
+            </el-select>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" icon="el-icon-search" @click="search(true)">查询</el-button>
             <el-button @click="resetSearch">重置</el-button>
           </el-form-item>
         </el-form>
+        <div class="outside-btn">
+          <el-button type="primary" size="small" class="add-btn" icon="el-icon-plus" @click="addClick">新增</el-button>
+        </div>
         <el-table :data="tableData" style="width: 100%" stripe>
           <el-table-column label="支付ID" prop="id" width="120" align="center"></el-table-column>
           <el-table-column width="100"></el-table-column>
@@ -38,7 +47,14 @@
       </div>
     </div>
     <!-- common-dialog -->
-    <edit-add-dialog v-if="dialogVisible" :isShow.sync="dialogVisible" :data="dialogData" :config="dialogConfig" @formSubmit="formSubmit"></edit-add-dialog>
+    <edit-add-dialog v-if="dialogVisible" :isShow.sync="dialogVisible" :data="dialogData" :config="dialogConfig" @formSubmit="formSubmit">
+      <!-- slot为true时显示 -->
+      <template slot="id" slot-scope="row">
+        <div>
+          {{row.data.id}}
+        </div>
+      </template>
+    </edit-add-dialog>
   </div>
 </template>
 
@@ -57,20 +73,22 @@ export default {
       currentPage: 1,
       searchForm: {
         name: "name",
-        value: ""
+        value: "",
+        status: ""
       },
       tableData: [],
       dialogVisible: false,
       dialogConfig: {
         title: { name: "", value: '' },
-        className:"",
+        className: "",
         data: [
           {
             label: "支付ID",
             prop: "id",
             placeholder: "请输入支付ID",
             type: "input",
-            disabled: true
+            disabled: true,
+            slot: false
           },
           {
             label: "支付名称",
@@ -134,15 +152,19 @@ export default {
       this.searchForm.value
         ? (params[this.searchForm.name] = this.searchForm.value)
         : "";
+      this.searchForm.status ? params.status = this.searchForm.status : "";
       params.page = this.currentPage;
       params.rows = this.size;
-      // console.log(params);
-      this.getMentList(params);
+      console.log('search', params);
+      // this.getMentList(params);
     },
     resetSearch() {
+      //特殊处理
       this.searchForm = {
         name: "name"
       };
+      //重置方法
+      this.$refs.searchForm.resetFields();
       this.currentPage = 1;
       this.size = 10;
       this.search();
@@ -215,19 +237,18 @@ export default {
         this.$message.error(params.title + "失败: " + res.msg);
       }
     },
-    getMentList(params) {
-      this.total = this.response.total;
-      this.tableData = this.response.rows;
-      console.log(params);
-      // MentList(params).then(res => {
-      //   // console.log(res);
-      //   this.total = res.total;
-      //   this.tableData = res.rows;
-      // });
-    }
+    // getMentList(params) {
+    //   MentList(params).then(res => {
+    //     // console.log(res);
+    //     this.total = res.total;
+    //     this.tableData = res.rows;
+    //   });
+    // }
   },
   mounted() {
-    this.getMentList({ page: this.currentPage, rows: this.size });
+    // this.getMentList({ page: this.currentPage, rows: this.size });
+    this.total = this.response.total;
+    this.tableData = this.response.rows;
   },
   watch: {
     total() {
@@ -242,29 +263,16 @@ export default {
 
 <style lang="scss" scoped>
 .pay-way-box {
-  .add-btn {
-    position: absolute;
-    right: 32px;
-    top: 20px;
-    padding: 10px 30px;
+  .outside-btn {
+    height: 40px;
+    line-height: 40px;
+    margin-bottom: 5px;
   }
   .search-form {
     margin-top: 20px;
   }
   .input-with-select .el-input-group__prepend .el-select {
     width: 100px;
-  }
-}
-</style>
-<style lang="scss">
-.payway-edit-dialog,
-.payway-add-dialog {
-  .el-dialog {
-    width: 600px;
-    .el-input__inner,
-    .el-input-number {
-      width: 300px;
-    }
   }
 }
 </style>
